e6fc710bacf02f467cd1e47bbfc9ed1cb34bd02e
tayrob5 <taylorrobinette93@yahoo.com>
2014-09-16 10:06:27 -0400

    playing with native camera

---
+++ js/app.js
@@ -0,0 +1,15 @@
+function launchCamera(){
+
+navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
+    destinationType: Camera.DestinationType.DATA_URL
+});
+
+}
+function onSuccess(imageData) {
+    var image = document.getElementById('myImage');
+    image.src = "data:image/jpeg;base64," + imageData;
+}
+
+function onFail(message) {
+    alert('Failed because: ' + message);
+}
