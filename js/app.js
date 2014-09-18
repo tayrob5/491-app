f624e95fabf9051a247e5d9ab98f538d7c750ee6
tayrob5 <taylorrobinette93@yahoo.com>
2014-09-18 10:34:10 -0400

    fun with maps

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
+document.addEventListener("deviceready", onDeviceReady, false);
+
+function onDeviceReady() {
+	console.log("onDeviceReady()");
+	navigator.geolocation.getCurrentPosition(generateMap, onError);
+}
+
 function capturePhoto(){
 
 navigator.camera.getPicture(uploadPhoto, null, { sourceType:1,quality: 50});
