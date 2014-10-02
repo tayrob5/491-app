1953647424de6e07436a29dab034c931251147dd
tayrob5 <taylorrobinette93@yahoo.com>
2014-10-02 09:57:29 -0400

    new camera launcher etc

---
+++ js/app.js
@@ -0,0 +1,19 @@
+function capturePhoto(){
+        //alert("capture button working");
+    navigator.camera.getPicture(uploadPhoto,null,{sourceType:3,quality:60});
+}   
+
+function uploadPhoto(data){
+// this is where you would send the image file to server
+
+
+    cameraPic.src = data;
+	//cameraPic.src = "data:image/jpeg;base64," + data;
+	// Successful upload to the server
+	navigator.notification.alert(
+		'Your Photo has been uploaded',  // message
+		okay,                           // callback
+	    'Photo Uploaded',              // title
+	    'OK'                          // buttonName
+	);
+}// JavaScript Document
