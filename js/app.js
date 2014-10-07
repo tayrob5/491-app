336a3cac079c1d20c4a69d5b23a54127736242f2
tayrob5 <taylorrobinette93@yahoo.com>
2014-10-07 02:22:54 -0400

    changing camera settings

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 function capturePhoto(){
         //alert("capture button working");
-    navigator.camera.getPicture(uploadPhoto,null,{sourceType:0,quality:60});
+    navigator.camera.getPicture(uploadPhoto,null,{sourceType:3,quality:60});
 }   
 
 function uploadPhoto(data){
